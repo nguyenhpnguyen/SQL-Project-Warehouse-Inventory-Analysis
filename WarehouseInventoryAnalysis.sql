4f6c6151086321aa758ea31399c2f49226e46d29
nguyenhpnguyen <e0817834@u.nus.edu>
2024-02-16 11:38:01 +0900

    Modified Tableau workbook

--- WarehouseInventoryAnalysis.sql
+++ WarehouseInventoryAnalysis.sql
@@ -8,12 +8,15 @@ SELECT products.warehouseCode, SUM(quantityInStock) AS quantityInStock, warehous
     ORDER BY warehousePctCap DESC;
 
 -- What is the real profit of the products in each warehouse?
--- The below query's result shows the actual profit from past orders,
+-- The below query's result shows the actual profit from past successful orders,
 -- ranking the warehouse based on the profit that their stored products generated.
 SELECT SUM(quantityOrdered*(priceEach-products.buyPrice)) AS profit, products.warehouseCode
 	FROM orderdetails
     LEFT JOIN products
-    ON orderdetails.productCode = products.productCode
+		ON orderdetails.productCode = products.productCode
+    LEFT JOIN orders
+		USING(orderNumber)
+    WHERE orders.status IN ("Resolved", "Shipped")
 	GROUP BY products.warehouseCode
     ORDER BY profit;
 
@@ -35,16 +38,20 @@ SELECT SUM(quantityOrdered / products.quantityInstock) AS totalOrderRate, produc
 -- What are the products that have not yielded the expected profit?
 -- The result shows the number of products with real profit less than suggested profit.
 -- The warehouses will be ranked based on how many undervalued products it stores.
-SELECT COUNT(DISTINCT products.productCode) AS undervaluedProducts, products.warehouseCode
-	FROM orderdetails
+SELECT COUNT(DISTINCT products.productCode) AS undervaluedProducts, p2.totalNoProducts, products.warehouseCode
+	FROM orderdetails 
     LEFT JOIN products
-    ON orderdetails.productCode = products.productCode
-    WHERE orderdetails.priceEach - products.buyPrice < products.MSRP - products.buyPrice
+		ON (orderdetails.productCode = products.productCode) AND (orderdetails.priceEach < products.MSRP)
+	JOIN (SELECT COUNT(DISTINCT productName) AS totalNoProducts, warehouseCode
+			FROM products
+            GROUP BY warehouseCode) AS p2
+		ON (p2.warehouseCode = products.warehouseCode)
     GROUP BY products.warehouseCode
     ORDER BY undervaluedProducts DESC;
 
--- What are the product that have not been moving at all?
-SELECT products.productCode, products.quantityInStock, products.warehouseCode, products.productLine
+
+-- What are the product that has not been moving at all during the 3 years recorded?
+SELECT products.productCode, products.productName, products.quantityInStock, products.warehouseCode, products.productLine
 	FROM products
     LEFT OUTER JOIN orderdetails
     ON products.productCode = orderdetails.productCode
